c28de9d9b36ba4284d0a9df5568a113af04571ea
reichardn <reichardn@gmail.com>
2016-08-26 15:40:20 -0400

    updated show deal view to list documents, separated by open versus closed

--- app/assets/javascripts/angular-app/controllers/DealController.js
+++ app/assets/javascripts/angular-app/controllers/DealController.js
@@ -2,7 +2,21 @@ function DealController(deal, DealsService) {
     
     var ctrl = this;
     ctrl.data = deal.data;
+    ctrl.open = [];
+    ctrl.finished = [];
 
+    this.splitDocs = function() {
+      var docs = ctrl.data.documents
+      for (var i = 0; i < docs.length; i++) {
+        if (docs[i].complete) {
+          ctrl.finished.push(docs[i])
+        } else {
+          ctrl.open.push(docs[i])
+        }
+      }
+    };
+
+    this.splitDocs();
 }
  
 angular
